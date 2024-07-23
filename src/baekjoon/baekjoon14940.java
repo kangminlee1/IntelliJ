8a0d87000fac2f76bce79db54bfa79f98b34dd32
kangminlee1 <jack7853@naver.com>
2024-07-23 23:56:47 +0900

    bfs 활용한 것인데 내가 한 풀의와 차이를 모르겠다. 다른 사람이 푼 것과 같은데 내 것은 틀렸다 나옴 수정해보자
    -> g해결

--- src/baekjoon/baekjoon14940.java
+++ src/baekjoon/baekjoon14940.java
@@ -1,6 +1,5 @@
 package baekjoon;
 
-//import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,66 +8,49 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class baekjoon14940 {
-/*
-    static class Point {
-        public int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-        //    static int start_row, start_col;
-        static int row, col;
-        static int[][] map;
-//        static int[][] distance;
-        static boolean[][] check;
-
-        static int[] dx = {-1, 1, 0, 0};
-        static int[] dy = {0, 0, 1, -1};
+    //        static int[][] distance;
+    static int row, col;
+    static boolean[][] check;
+    static int[][] map;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
 
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            StringBuffer sb = new StringBuffer();
-
+            StringBuilder sb = new StringBuilder();
+            StringTokenizer st;
+            st = new StringTokenizer(br.readLine());
             row = Integer.parseInt(st.nextToken());
             col = Integer.parseInt(st.nextToken());
+            map= new int[row][col];
 
-            map = new int[row][col];
-//            distance = new int[row][col];
-            check = new boolean[row][col];
-
-            int start_row = -1, start_col = -1;
-
+            int start_x = 0;
+            int start_y = 0;
             for (int i = 0; i < row; i++) {
                 st = new StringTokenizer(br.readLine());
-
                 for (int j = 0; j < col; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                     if (map[i][j] == 2) {
-                        map[i][j]= 0;
-                        start_row = i;
-                        start_col = j;
+                        map[i][j] = 0;
+                        start_x = i;
+                        start_y = j;
                     }
+
                 }
             }
+            check = new boolean[row][col];
+            bfs(start_x, start_y);
 
-            bfs(start_row, start_col);
-
-            for(int i = 0; i < row; i++){
-                for(int j = 0; j < col; j++){
-                    if(!check[i][j] && map[i][j] == 1){
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (map[i][j] == 1 && !check[i][j]) {
                         map[i][j] = -1;
                     }
-                    sb.append(map[i][j]+ " ");
+                    sb.append(map[i][j]+" ");
                 }
                 sb.append("\n");
             }
-
-            System.out.println(sb);
+            System.out.print(sb);
 
         }
 
@@ -84,29 +66,35 @@ public class baekjoon14940 {
                 //해당 점을 뽑아내는 방법
 
                 for (int i = 0; i < 4; i++) {
-                    int corrent_row = temp.x - dx[i];
-                    int corrent_col = temp.y - dy[i];
+                    int n_row = temp.x - dx[i];
+                    int n_col = temp.y - dy[i];
 
-                    if (corrent_row < 0 || corrent_col < 0 || corrent_row >= row || corrent_col >= col) {
+
+//                    if (n_row < 0 || n_col < 0 || n_row >= row || n_col >= col)
+//                        continue;
+//                    if (check[n_row][n_col] || map[n_row][n_col] == 0)
+//                        continue;
+
+                    if (n_row < 0 || n_col < 0 || n_row >= row || n_col >= col) {
                         continue;
                     }
                     //배열 범위를 벗어나면 pass
 
-//                    if (map[corrent_row][corrent_col] == 0) {
-//                        continue;
-//                    }
+                    if (map[n_row][n_col] == 0) {
+                        continue;
+                    }
                     //0인 곳은 방문하지 못함
 
-                    if (check[corrent_row][corrent_col]) {
+                    if (check[n_row][n_col]) {
                         continue;
                     }
                     //방문 했으면 pass
 
                         //방문하지 않았고 갈 수 있는 점이일 경우
-                        que.add(new Point(corrent_row, corrent_col));
-                        check[corrent_row][corrent_col] = true;
+                        que.add(new Point(n_row, n_col));
+                        check[n_row][n_col] = true;
                         //그 점을 Queue에 저장하고 방문했으니 true로 설정
-                        map[corrent_row][corrent_col] = map[temp.x][temp.y] + 1;
+                        map[n_row][n_col] = map[temp.x][temp.y] + 1;
 
 
                 }
@@ -114,9 +102,17 @@ public class baekjoon14940 {
             }//while문 끝
 
         }//bfs의 끝
-
 }
-*/
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+    /*
 static int N, M;
     static boolean[][] visited;
     static int[][] arr;
@@ -188,3 +184,5 @@ class Node {
         this.y = y;
     }
 }
+*/
+
